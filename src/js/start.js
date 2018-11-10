6e71156b042476644d1460fe8363818bf2266543
Christian Vari <vari.christian@gmail.com>
2018-11-10 10:07:07 +0100

    Using Modules ES6 and modularized three library.

--- src/js/start.js
+++ src/js/start.js
@@ -1,3 +1,49 @@
-import * as start from "./init.js"
+//Import
 
-start.init()
+import * as THREE from "./lib/three.module.js";
+
+//Global Variables
+
+var scene, camera, renderer, cube;
+
+//Functions
+
+function inizialize(){
+
+
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    camera.position.z = 5;
+
+}
+
+function createCube(){
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    return cube;
+}
+
+function animate() {
+    requestAnimationFrame( animate );
+    
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+}
+
+function main (){
+    inizialize();
+    createCube();
+    animate();
+}
+
+main(); //GO
