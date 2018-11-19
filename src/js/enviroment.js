ac4c087edf6a8eda3f07ec7dbde23ef4f4e1f6b8
Christian Vari <vari.christian@gmail.com>
2018-11-19 16:46:51 +0100

    possibly fixed go down right

--- src/js/enviroment.js
+++ src/js/enviroment.js
@@ -132,9 +132,7 @@ export class Enviroment {
 		if(this.myscene.player.isOnTheSecondLevel && this.player_on_obstacle_index == i){
 
 
-			if((this.myscene.player.getPositionZ() + (PLAYER_EDGE/2)  < this.myscene.getObstacleTailPositionZ(i))
-
-				|| (this.myscene.player.getPositionX() != this.myscene.getObstaclePositionX(i))) {
+			if((this.myscene.player.getPositionZ() + (PLAYER_EDGE/2)  < this.myscene.getObstacleTailPositionZ(i))) {
 
 
 				console.log("SCENDO");
@@ -142,6 +140,26 @@ export class Enviroment {
 				this.myscene.player.going_down = true;
 				this.myscene.player.isOnTheSecondLevel=false;
 				this.player_on_obstacle_index = -1;
+			}
+			else if(this.myscene.player.getPositionX() != this.myscene.getObstaclePositionX(i)){
+
+				console.log(this.myscene.player.command + " "+
+				(this.myscene.player.getPositionX() /*- (PLAYER_EDGE/2)*/+ " > " + this.myscene.getObstaclePositionX(i) + (PLAYER_EDGE/2)));
+
+
+				if((((this.myscene.player.command == 0|| this.myscene.player.command == 1)) && 
+				(this.myscene.player.getPositionX() /*- (PLAYER_EDGE/2)*/ > this.myscene.getObstaclePositionX(i)+ (PLAYER_EDGE/2)))
+				|| (((this.myscene.player.command == 2|| this.myscene.player.command == 3)) && 
+				(this.myscene.player.getPositionX() + (PLAYER_EDGE/2) < this.myscene.getObstaclePositionX(i) - (PLAYER_EDGE/2)))
+				){
+
+					console.log("SCENDO DI LATO)");
+
+					this.myscene.player.going_down = true;
+					this.myscene.player.isOnTheSecondLevel=false;
+					this.player_on_obstacle_index = -1;
+				}
+
 			}
 
 		}
