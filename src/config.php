2d62d778464beb91fe632cfb239f18329e4fea09
Christian Vari <vari.christian@gmail.com>
2018-12-01 16:35:41 +0100

    server online

--- src/config.php
+++ src/config.php
@@ -1,7 +1,7 @@
 <?php
    define('DB_SERVER', 'localhost');
-   //define('DB_USERNAME', 'shapes');
-   define('DB_USERNAME', 'root');
+   define('DB_USERNAME', 'shapes');
+   //define('DB_USERNAME', 'root');
    define('DB_PASSWORD', '');
    define('DB_DATABASE', 'my_shapes');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
