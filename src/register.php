5afbbf25afca014de1cd6a4efafea1409b65c130
Christian Vari <vari.christian@gmail.com>
2018-12-07 16:47:48 +0100

    beautify errors

--- src/register.php
+++ src/register.php
@@ -83,7 +83,7 @@
                 <?php  if (count($errors) > 0) : ?>
                     <div class="error">
                     <?php foreach ($errors as $error) : ?>
-  	                    <p><?php echo $error ?></p>
+  	                    <p class="form-control-feedback"><?php echo $error ?></p>
   	                <?php endforeach ?>
                     </div>
                 <?php  endif ?>
