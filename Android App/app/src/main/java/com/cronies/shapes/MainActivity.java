77137d596b9270aa178ddd5ccb5e1fde52f00d80
MarcoPenne <marco.pennese.97@gmail.com>
2018-12-12 22:47:38 +0100

    added Android App

---
+++ Android App/app/src/main/java/com/cronies/shapes/MainActivity.java
@@ -0,0 +1,98 @@
+package com.cronies.shapes;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        //WebView myWebView = (WebView) findViewById(R.id.webview);
+
+        myWebView = new WebView(this.getApplicationContext());
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN );
+
+        //GestureDetector gestureDetector = new GestureDetector(this.getApplicationContext(),  new GestureDetector.OnGestureListener().on);
+        updateUI();
+        myWebView.loadUrl("http://shapes.altervista.org/");
+        WebSettings webSettings = myWebView.getSettings();
+        //webSettings.setUserAgentString("");
+        webSettings.setJavaScriptEnabled(true);
+        setContentView(myWebView);
+
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        try {
+            Class.forName("android.webkit.WebView")
+                    .getMethod("onPause", (Class[]) null)
+                    .invoke(myWebView, (Object[]) null);
+
+        } catch(Exception e) {
+            System.out.println("PROBLEMA onPause()");
+        }
+
+
+
+    }
+
+    public void updateUI() {
+        final View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        decorView.setOnSystemUiVisibilityChangeListener (new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+
+                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                    decorView.setSystemUiVisibility(
+
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        myWebView.onResume();
+        updateUI();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        myWebView.loadUrl("about:blank");
+
+        super.onDestroy();
+
+    }
+}
